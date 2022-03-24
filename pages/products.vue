7c1895666ced06cf0412abb5e242a58d14e7c155
satoshi aikawa <satoshiaikawa@192.168.0.102>
2022-03-24 17:42:42 +0900

    ok

--- pages/products.vue
+++ pages/products.vue
@@ -1,63 +1,61 @@
 <template>
-    <v-container fluid>
-        <v-row>
-            <v-col>
-                <v-textarea
-                    v-model="text"
-                    hint="Text here"
-                    ></v-textarea>
-                <br />
-                <v-btn @click="onSubmit">
-                    Submit
-                </v-btn>
-            </v-col>
-        </v-row>
-        <v-row>
-            <v-col>
-                <Note
-                    :text="shown_text"
-                    @clear="onClear"
-                    />
-            </v-col>
-        </v-row>
+  <div>
+    <v-container
+      v-for="align in alignments"
+      :key="align"
+      class="grey lighten-5 mb-6"
+    >
+      <v-row
+        :align="align"
+        no-gutters
+        style="height: auto"
+      >
+        <v-col
+          v-for="n in 3"
+          :key="n"
+        >
+          <v-card
+            class="pa-2"
+            outlined
+            tile
+          >
+            <Product />
+          </v-card>
+        </v-col>
+      </v-row>
     </v-container>
+
+    <v-container class="grey lighten-5">
+      <v-row
+        no-gutters
+        style="height: 150px;"
+      >
+        <v-col
+          v-for="align in alignments"
+          :key="align"
+          :align-self="align"
+        >
+          <v-card
+            class="pa-2"
+            outlined
+            tile
+          >
+            One of three columns
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref, onMounted, useContext} from '@nuxtjs/composition-api'
-import { NuxtAxiosInstance } from '@nuxtjs/axios'
-
-export default defineComponent({
-    setup() {
-        const products = ref<any[]>([])
-
-        const getUserRepositories = async () => {
-            //const response = await NuxtAxiosInstance.$get('https://httpbin.org/get');
-            //console.log(response)
-
-        }
-
-        const onSubmit = () => {
-            console.log("onsubmit")
-        }
-
-        const onClear = () => {
-            console.log("onclear")
-        }
-
-        const { $config } = useContext()
-
-        onMounted(() => {
-            console.log('mounted!')
-            console.log($config.stage)
-        })
-
-        return {
-            products,
-            onSubmit,
-            onClear,
-        }
-    },
-    layout: 'note',
-})
+<script>
+  export default {
+    data: () => ({
+      alignments: [
+        'start',
+        'center',
+        'end',
+      ],
+    }),
+  }
 </script>
